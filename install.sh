4e93b8feb2656b69bce87e0a2085916b7c1182ba
Kevin Jalbert <kevin.j.jalbert@gmail.com>
2012-04-23 14:42:52 -0400

    Bump ARC version
    Update:
     * ARC version

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 echo "::Acquiring ARC from GitHub (Using SSH, requires access and credentials)"
 git clone git@github.com:sqrg-uoit/arc.git test_area/arc
 cd test_area/arc
-git checkout 89207b53cbb6a95f633e1162e027a73c7a7c5c93  # May not be required eventually
+git checkout f37a01f2edafa012d449e1b2452e8c348aea4734  # May not be required eventually
 cd ../..
 
 mkdir test_area/arc/lib
